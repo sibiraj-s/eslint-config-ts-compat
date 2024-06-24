b7214b1d48f28df5365beac431ffcb56180b220a
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2024-06-24 13:04:19 +0530

    Fix docs links

--- index.js
+++ index.js
@@ -3,107 +3,107 @@ export default {
   rules: {
     // enforce consistent brace style for blocks
     // https://eslint.org/docs/rules/brace-style
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/brace-style.md
+    // https://typescript-eslint.io/rules/brace-style
     'brace-style': 'off',
 
     // enforce camelcase naming convention
     // https://eslint.org/docs/rules/camelcase
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming-convention.md
+    // https://typescript-eslint.io/rules/naming-convention
     'camelcase': 'off',
 
     // enforce consistent spacing before and after commas
     // https://eslint.org/docs/rules/comma-spacing
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/comma-spacing.md
+    // https://typescript-eslint.io/rules/comma-spacing
     'comma-spacing': 'off',
 
     // enforce default parameters to be last
     // https://eslint.org/docs/rules/default-param-last
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/default-param-last.md
+    // https://typescript-eslint.io/rules/default-param-last
     'default-param-last': 'off',
 
     // enforce dot notation whenever possible
     // https://eslint.org/docs/rules/dot-notation
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/dot-notation.md
+    // https://typescript-eslint.io/rules/dot-notation
     'dot-notation': 'off',
 
     // require or disallow spacing between function identifiers and their invocations
     // https://eslint.org/docs/rules/func-call-spacing
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/func-call-spacing.md
+    // https://typescript-eslint.io/rules/func-call-spacing
     'func-call-spacing': 'off',
 
     // enforce consistent indentation
     // https://eslint.org/docs/rules/indent
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/indent.md
+    // https://typescript-eslint.io/rules/indent
     'indent': 'off',
 
     // require or disallow initialization in variable declarations
     // https://eslint.org/docs/rules/init-declarations
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/init-declarations.md
+    // https://typescript-eslint.io/rules/init-declarations
     'init-declarations': 'off',
 
     // enforce consistent spacing before and after keywords
     // https://eslint.org/docs/rules/keyword-spacing
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/keyword-spacing.md
+    // https://typescript-eslint.io/rules/keyword-spacing
     'keyword-spacing': 'off',
 
     // require or disallow an empty line between class members
     // https://eslint.org/docs/rules/lines-between-class-members
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/lines-between-class-members.md
+    // https://typescript-eslint.io/rules/lines-between-class-members
     'lines-between-class-members': 'off',
 
     // disallow `Array` constructors
     // https://eslint.org/docs/rules/no-array-constructor
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-array-constructor.md
+    // https://typescript-eslint.io/rules/no-array-constructor
     'no-array-constructor': 'off',
 
     // disallow duplicate class members
     // https://eslint.org/docs/rules/no-dupe-class-members
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-dupe-class-members.md
+    // https://typescript-eslint.io/rules/no-dupe-class-members
     'no-dupe-class-members': 'off',
 
     // disallow empty functions
     // https://eslint.org/docs/rules/no-empty-function
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-empty-function.md
+    // https://typescript-eslint.io/rules/no-empty-function
     'no-empty-function': 'off',
 
     // disallow unnecessary parentheses
     // https://eslint.org/docs/rules/no-extra-parens
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-extra-parens.md
+    // https://typescript-eslint.io/rules/no-extra-parens
     'no-extra-parens': 'off',
 
     // disallow unnecessary semicolons
     // https://eslint.org/docs/rules/no-extra-semi
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-extra-semi.md
+    // https://typescript-eslint.io/rules/no-extra-semi
     'no-extra-semi': 'off',
 
     // disallow the use of `eval()`-like methods
     // https://eslint.org/docs/rules/no-implied-eval
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-implied-eval.md
+    // https://typescript-eslint.io/rules/no-implied-eval
     'no-implied-eval': 'off',
 
     // disallow this keywords outside of classes or class-like objects
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-invalid-this.md
     // https://eslint.org/docs/rules/no-invalid-this
+    // https://typescript-eslint.io/rules/no-invalid-this
     'no-invalid-this': 'off',
 
     // disallow magic numbers
     // https://eslint.org/docs/rules/no-magic-numbers
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-magic-numbers.md
+    // https://typescript-eslint.io/rules/no-magic-numbers
     'no-magic-numbers': 'off',
 
     // disallow unnecessary `return await`
     // https://eslint.org/docs/rules/no-return-await
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/return-await.md
+    // https://typescript-eslint.io/rules/return-await
     'no-return-await': 'off',
 
     // disallow variable declarations from shadowing variables declared in the outer scope
     // https://eslint.org/docs/rules/no-shadow
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-shadow.md
+    // https://typescript-eslint.io/rules/no-shadow
     'no-shadow': 'off',
 
     // disallow throwing literals as exceptions
     // https://eslint.org/docs/rules/no-throw-literal
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-throw-literal.md
+    // https://typescript-eslint.io/rules/no-throw-literal
     'no-throw-literal': 'off',
 
     // disallow Undeclared Variables
@@ -112,42 +112,42 @@ export default {
 
     // disallow unused expressions
     // https://eslint.org/docs/rules/no-unused-expressions
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-expressions.md
+    // https://typescript-eslint.io/rules/no-unused-expressions
     'no-unused-expressions': 'off',
 
     // disallow unused variables
     // https://eslint.org/docs/rules/no-unused-vars
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
+    // https://typescript-eslint.io/rules/no-unused-vars
     'no-unused-vars': 'off',
 
     // disallow early use
     // https://eslint.org/docs/rules/no-use-before-define
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md
+    // https://typescript-eslint.io/rules/no-use-before-define
     'no-use-before-define': 'off',
 
     // disallow unnecessary constructors
     // https://eslint.org/docs/rules/no-useless-constructor
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-useless-constructor.md
+    // https://typescript-eslint.io/rules/no-useless-constructor
     'no-useless-constructor': 'off',
 
     // enforce the consistent use of either backticks, double, or single quotes
     // https://eslint.org/docs/rules/quotes
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/quotes.md
+    // https://typescript-eslint.io/rules/quotes
     'quotes': 'off',
 
     // disallow async functions which have no `await` expression
     // https://eslint.org/docs/rules/require-await
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/require-await.md
+    // https://typescript-eslint.io/rules/require-await
     'require-await': 'off',
 
     // require or disallow semicolons instead of ASI
     // https://eslint.org/docs/rules/semi
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/semi.md
+    // https://typescript-eslint.io/rules/semi
     'semi': 'off',
 
     // enforce consistent spacing before `function` definition opening parenthesis
     // https://eslint.org/docs/rules/space-before-function-paren
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/space-before-function-paren.md
+    // https://typescript-eslint.io/rules/space-before-function-paren
     'space-before-function-paren': 'off',
   },
 };
